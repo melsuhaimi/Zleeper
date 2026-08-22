4d9ad018a17f852d582b2654666cb7a905d2f355
melsuhaimi <melsuhaimi95@gmail.com>
2026-08-22 17:34:34 +0800

    test: assert complete Room v2 table set

--- app/src/androidTest/kotlin/com/zleeper/sleepapp/RoomSchemaTest.kt
+++ app/src/androidTest/kotlin/com/zleeper/sleepapp/RoomSchemaTest.kt
@@ -10,11 +10,20 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class RoomSchemaTest {
-    @Test fun versionOneCreatesAllEighteenSourceOfTruthTables() {
-        val database = Room.inMemoryDatabaseBuilder(ApplicationProvider.getApplicationContext(), ZleeperDatabase::class.java).build()
+    @Test fun versionTwoCreatesAllSourceOfTruthTables() {
+        val database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            ZleeperDatabase::class.java,
+        ).build()
         database.openHelper.writableDatabase.query("SELECT name FROM sqlite_master WHERE type='table'").use { cursor ->
             val names = buildSet { while (cursor.moveToNext()) add(cursor.getString(0)) }
-            val expected = setOf("sleep_session", "sleep_signal", "night_outcome", "expedition", "expedition_path_node", "expedition_reward", "pet", "pet_progression_event", "inventory_stack", "inventory_instance", "inventory_transaction", "equipment_slot", "quest_progress", "quest_objective_progress", "world_unlock", "world_discovery", "collection_entry", "morning_note")
+            val expected = setOf(
+                "sleep_session", "sleep_signal", "night_outcome", "expedition", "expedition_path_node", "expedition_reward",
+                "pet", "pet_progression_event", "inventory_stack", "inventory_instance", "inventory_transaction", "equipment_slot",
+                "equipment_progression_event", "quest_progress", "quest_objective_progress", "world_unlock", "world_discovery",
+                "collection_entry", "morning_note", "pet_specialization", "pet_memory", "hearth_progress", "hearth_progression_event",
+                "title_unlock", "scene_completion",
+            )
             assertEquals(expected, names.intersect(expected))
         }
         database.close()
